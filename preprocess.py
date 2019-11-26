28be357a5ebbca918c1989af36b95e95dd98620a
michaeltlincoln <michaeltlincoln@gmail.com>
2019-11-26 12:37:51 -0500

    Add requirements.txt and optimize preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import pandas as pd
 
 PAD_TOKEN = "*PAD*"
 UNK_TOKEN = "*UNK*"
@@ -22,62 +23,33 @@ def read_data(train_file, test_inputs_file, test_labels_file):
     """
     
     # labels stored as toxic, severe_toxic, obscene, threat, insult, identity_hate
-    train_inputs = []
-    train_labels = []
-    test_inputs = []
-    test_labels = []
-    word_counts = {}
-    with open(train_file) as train_f:
-        csv_reader = csv.reader(train_f, delimiter=',')
-        next(csv_reader)
-        for row in csv_reader:
-            train_inputs.append(row[1])
-            for word in row[1].split():
-                if word not in word_counts:
-                    word_counts[word] = 1
-                else:
-                    word_counts[word] += 1
-            label = np.array(list(map(int, row[2:8])))
-            train_labels.append(label)
-    
-    tests_to_remove = set()
-    with open(test_labels_file) as test_labels_f:
-        csv_reader = csv.reader(test_labels_f, delimiter=',')
-        next(csv_reader)
-        line_number = 0
-        for row in csv_reader:
-            label = np.array(list(map(int, row[1:7])))
-            if np.any(label == -1):
-                tests_to_remove.add(line_number)
-            else:
-                test_labels.append(label)
-            line_number += 1
+    train = pd.read_csv(train_file)
+    test_inputs = pd.read_csv(test_inputs_file)
+    test_labels = pd.read_csv(test_labels_file)
+    test = pd.merge(test_inputs, test_labels, on="id")
+    test = test[(test[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']] != -1).all(axis=1)]
+    return train, test
 
-    with open(test_inputs_file) as test_inputs_f:
-        csv_reader = csv.reader(test_inputs_f, delimiter=',')
-        next(csv_reader)
-        line_number = 0
-        for row in csv_reader:
-            if line_number not in tests_to_remove:
-                test_inputs.append(row[1])
-                for word in row[1].split():
-                    if word not in word_counts:
-                        word_counts[word] = 1
-                    else:
-                        word_counts[word] += 1
-            line_number += 1
-    
-    train_labels = np.array(train_labels)
-    test_labels = np.array(test_labels)
-    return train_inputs, train_labels, test_inputs, test_labels, word_counts
-
-def build_vocab(word_counts):
+def build_vocab(train, test):
     """
     Builds vocab a word count dictionary. Removes rare words and adds UNK and PAD tokens.
 
     :param word_counts:  Dict containg word->word count
     :return: tuple of (dictionary: word --> unique index, pad_token_idx)
     """
+    word_counts = {}
+    for comment in train['comment_text']:
+        for word in comment.split():
+            if word not in word_counts:
+                word_counts[word] = 1
+            else:
+                word_counts[word] += 1
+    for comment in test['comment_text']:
+        for word in comment.split():
+            if word not in word_counts:
+                word_counts[word] = 1
+            else:
+                word_counts[word] += 1
     current_id = 0
     vocab_dict = {}
     for word, count in word_counts.items():
@@ -86,7 +58,7 @@ def build_vocab(word_counts):
             current_id += 1
     vocab_dict[UNK_TOKEN] = current_id
     vocab_dict[PAD_TOKEN] = current_id + 1
-    return vocab_dict, current_id + 1
+    return vocab_dict
 
 def pad_corpus(comments):
     """
@@ -128,13 +100,16 @@ def get_data(train_file, test_inputs_file, test_labels_file):
     2-d numpy array with testing labels form [num_comments x 6]
     vocab (Dict containg word->index mapping)
     """
-    train_inputs, train_labels, test_inputs, test_labels, word_counts = read_data(train_file, test_inputs_file, test_labels_file)
-    vocab_dict, pad_index = build_vocab(word_counts)
+    train, test = read_data(train_file, test_inputs_file, test_labels_file)
+    vocab_dict = build_vocab(train, test)
     
-    train_inputs = pad_corpus(train_inputs)
-    test_inputs = pad_corpus(test_inputs)
+    train_inputs = pad_corpus(train['comment_text'])
+    test_inputs = pad_corpus(test['comment_text'])
     
     train_inputs = convert_to_id(vocab_dict, train_inputs)
     test_inputs = convert_to_id(vocab_dict, test_inputs)
+    
+    train_labels = np.array(train[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']])
+    test_labels = np.array(test[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']])
     
     return train_inputs, train_labels, test_inputs, test_labels, vocab_dict
